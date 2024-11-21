0a8be2bda913f8803125848c083722672509e12b
allenjiang <allenjianggo@gmail.com>
2024-11-21 23:33:54 +0800

    build: update layers

--- variables.tf
+++ variables.tf
@@ -34,12 +34,12 @@ variable "php_lambda_layer_arn" {
   type = string
   # check all php layer runtime in this page
   # https://runtimes.bref.sh/?region=us-west-2&version=2.3.9
-  default = "arn:aws:lambda:us-west-2:534081306603:layer:arm-php-83:38"
+  default = "arn:aws:lambda:us-west-2:534081306603:layer:arm-php-83:39"
 }
 
 variable "console_lambda_layer_arn" {
   type    = string
-  default = "arn:aws:lambda:us-west-2:534081306603:layer:console:91"
+  default = "arn:aws:lambda:us-west-2:534081306603:layer:console:92"
 }
 
 # laravel settings
