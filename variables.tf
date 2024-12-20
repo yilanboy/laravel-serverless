f4c85fc04fd7ac22fa0ac0ed8c096581610b59fe
allenjiang <allenjianggo@gmail.com>
2024-12-20 23:12:46 +0800

    feat: update layers

--- variables.tf
+++ variables.tf
@@ -38,12 +38,12 @@ variable "php_lambda_layer_arn" {
   type = string
   # check all php layer runtime in this page
   # https://runtimes.bref.sh/?region=us-west-2
-  default = "arn:aws:lambda:us-west-2:534081306603:layer:arm-php-84:13"
+  default = "arn:aws:lambda:us-west-2:534081306603:layer:arm-php-84:14"
 }
 
 variable "console_lambda_layer_arn" {
   type    = string
-  default = "arn:aws:lambda:us-west-2:534081306603:layer:console:94"
+  default = "arn:aws:lambda:us-west-2:534081306603:layer:console:95"
 }
 
 variable "enable_vpc" {
