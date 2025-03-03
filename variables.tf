73e9d32f4cc3fd591233e6d339362fb17e605dd7
Allen <allenjianggo@gmail.com>
2025-03-03 16:45:48 +0800

    chore: update layer version

--- variables.tf
+++ variables.tf
@@ -38,12 +38,12 @@ variable "php_lambda_layer_arn" {
   type = string
   # check all php layer runtime in this page
   # https://runtimes.bref.sh/?region=us-west-2
-  default = "arn:aws:lambda:us-west-2:534081306603:layer:arm-php-84:15"
+  default = "arn:aws:lambda:us-west-2:534081306603:layer:arm-php-84:18"
 }
 
 variable "console_lambda_layer_arn" {
   type    = string
-  default = "arn:aws:lambda:us-west-2:534081306603:layer:console:96"
+  default = "arn:aws:lambda:us-west-2:534081306603:layer:console:99"
 }
 
 variable "enable_vpc" {
