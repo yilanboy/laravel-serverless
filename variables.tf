1ecdd67c99f6f6482be9f5f596ce7c56a28f346b
allenjiang <allenjianggo@gmail.com>
2025-06-07 00:06:32 +0800

    build: Bump PHP and Console Lambda Layers to the latest versions

--- variables.tf
+++ variables.tf
@@ -38,12 +38,12 @@ variable "php_lambda_layer_arn" {
   type = string
   # check all php layer runtime in this page
   # https://runtimes.bref.sh/?region=us-west-2
-  default = "arn:aws:lambda:us-west-2:534081306603:layer:arm-php-84:24"
+  default = "arn:aws:lambda:us-west-2:534081306603:layer:arm-php-84:26"
 }
 
 variable "console_lambda_layer_arn" {
   type    = string
-  default = "arn:aws:lambda:us-west-2:534081306603:layer:console:105"
+  default = "arn:aws:lambda:us-west-2:534081306603:layer:console:107"
 }
 
 variable "enable_vpc" {
