fa6beb9d4506499b9d86d21d4a7d734c6dda2726
Allen <allenjianggo@gmail.com>
2025-06-19 16:36:48 +0800

    Add aws provider version constraint

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "us-west-2-terraform-state-storage"
     key            = "us-west-2-blog-serverless.tfstate"
