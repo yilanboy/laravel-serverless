0abf28fad88edb39d5b2fcc81a25364e0f47939b
Allen <allenjianggo@gmail.com>
2025-08-11 13:56:43 +0800

    feat: configure terraform backend with dynamic settings and ignore config file

--- terraform.tf
+++ terraform.tf
@@ -6,10 +6,5 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "us-west-2-terraform-state-storage"
-    key            = "us-west-2-blog-serverless.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "us-west-2-terraform-state-locking"
-  }
+  backend "s3" {}
 }
