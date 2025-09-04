291e224413528b48ba427c0beefaaf2966698533
Allen <allenjianggo@gmail.com>
2025-09-04 14:25:58 +0800

    fix: update Lambda layer ARNs for PHP and console to latest versions

--- variables.tf
+++ variables.tf
@@ -34,12 +34,12 @@ variable "php_lambda_layer_arn" {
   type = string
   # check all php layer runtime in this page
   # https://runtimes.bref.sh/?region=us-west-2
-  default = "arn:aws:lambda:us-west-2:534081306603:layer:arm-php-84:29"
+  default = "arn:aws:lambda:us-west-2:534081306603:layer:arm-php-84:31"
 }
 
 variable "console_lambda_layer_arn" {
   type    = string
-  default = "arn:aws:lambda:us-west-2:534081306603:layer:console:110"
+  default = "arn:aws:lambda:us-west-2:534081306603:layer:console:112"
 }
 
 variable "enable_vpc" {
